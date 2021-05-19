687a9de0008c81170544518fc8c7dd58109db6ee
andreea <andreea.leu99@gmail.com>
2021-05-19 13:53:01 +0300

    pls

--- post_log.php
+++ post_log.php
@@ -1,25 +1,25 @@
 <?php
 
-    // var_dump($_POST);
-    // $type = $_POST["type"];
-	// $severity = $_POST["severity"];
-	// $date = $_POST["date"];
-	// $message = $_POST["message"];
-    // $user= "andreea";
+    var_dump($_POST);
+    $type = $_POST["type"];
+	$severity = $_POST["severity"];
+	$date = $_POST["date"];
+	$message = $_POST["message"];
+    $user= "andreea";
     
 
-    // include_once 'includes/dbh.inc.php';
+    include_once 'includes/dbh.inc.php';
 
-    // if ($conn->connect_error) 
-    // {
-    //     alert("error");
-    //     die("Connection failed: " . $conn->connect_error);
-    // }
+    if ($conn->connect_error) 
+    {
+        alert("error");
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-    // echo "here";
-    // $stmt = $conn->prepare("INSERT INTO log_report (type, severity, date, message, user) VALUES (?, ?, ?, ?, ?)");
-    // $stmt->bind_param("sssss", $type, $severity, $date, $message, $user);
-    // $stmt->execute();
-    // $stmt->close();
-	// $conn->close();
+    echo "here";
+    $stmt = $conn->prepare("INSERT INTO log_report (type, severity, date, message, user) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $type, $severity, $date, $message, $user);
+    $stmt->execute();
+    $stmt->close();
+	$conn->close();
 ?>
